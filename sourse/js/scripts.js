fd1a7658069c1258c2579cfb3b80d21e7d7f83a2
ligalaiz <slavakuz88@gmail.com>
2018-11-15 21:31:57 +0300

    Верстка contacts, page-footer, добавление y-api

--- sourse/js/scripts.js
+++ sourse/js/scripts.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var toggle = document.querySelector(".page-toggle");
 var mainNav = document.querySelector(".main-nav");
 
@@ -8,3 +10,31 @@ toggle.addEventListener("click", function () {
   mainNav.classList.toggle("main-nav--show");
   toggle.classList.toggle("page-toggle--close");
 });
+
+(function() {
+
+ymaps.ready(init);
+
+function init () {
+  var myMap;
+  myMap = new ymaps.Map('map', {
+    center:[59.939015, 30.323200],
+    zoom:17,
+    controls: []
+  });
+
+  myMap.behaviors.disable('scrollZoom');
+  myMap.controls.add("zoomControl", {
+    position: {top: 10, left: 15}
+  });
+
+  var myPlacemark = new ymaps.Placemark([59.938730, 30.323086] , {},
+      { iconLayout: 'default#image',
+        iconImageHref: 'img/map-pin.png',
+        iconImageSize: [55, 53],
+        iconImageOffset: [-15, -90]
+      });
+
+  myMap.geoObjects.add(myPlacemark);
+}
+})();
